d9201a6e90aaa6c54b1f6cbc4ee2d959e9631746
caren1 <czarnocki.w@wp.pl>
2021-04-28 19:05:32 +0200

    using images from contentful

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function RecipeCard({ recipe }) {
 
     const { title, slug, cookingTime, thumbnail } = recipe.fields;
+    
     return (
         <div className="card">
             <div className="featured">
-                {/* img + thumb  */}
+                <Image src={`https:${thumbnail.fields.file.url}`} width={thumbnail.fields.file.details.image.width} height={thumbnail.fields.file.details.image.height}/>
             </div>
             <div className="content">
                 <div className="info">
