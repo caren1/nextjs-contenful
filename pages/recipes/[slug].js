6a06fe856c002b4358763e7145818c3290fa7e40
caren1 <czarnocki.w@wp.pl>
2021-04-28 19:48:57 +0200

    adjustment

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful'
-import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -8,15 +8,13 @@ const client = createClient({
 })
 
 export const getStaticPaths = async () => {
-  const res = await client.getEntries({
-    content_type: 'recipe',
+  const res = await client.getEntries({ 
+    content_type: "recipe" 
   })
 
-  const paths = res.items.map((item) => {
+  const paths = res.items.map(item => {
     return {
-      params: {
-        slug: item.fields.slug
-      }
+      params: { slug: item.fields.slug }
     }
   })
 
@@ -26,86 +24,72 @@ export const getStaticPaths = async () => {
   }
 }
 
-export async function getStaticProps ({ params }) {
-
+export const getStaticProps = async ({ params }) => {
   const { items } = await client.getEntries({
     content_type: 'recipe',
-    'fields.slug':params.slug
+    'fields.slug': params.slug
   })
 
   return {
-    props: {
-      recipe: items[0]
-    }
+    props: { recipe: items[0] }
   }
+
 }
 
 export default function RecipeDetails({ recipe }) {
-
-  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
+  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
+  console.log(method)
 
   return (
-    <div className="card">
+    <div>
       <div className="banner">
         <Image 
-        src={`https:${featuredImage.fields.file.url}`}
-        width={featuredImage.fields.file.details.image.width}
-        height={featuredImage.fields.file.details.image.height}
+          src={'https:' + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
         />
-        <h2>{title}</h2>
-      </div>
-      <div className="info">
-        <p>Takes about {cookingTime} mins to cook.</p>
-        <h3>ingredients :</h3>
-        
-          {ingredients.map(ingredient => (
-            <span key={ingredient}>
-              {ingredient}
-            </span>
-          ))}
+        <h2>{ title }</h2>
       </div>
 
+      <div className="info">
+        <p>Takes about { cookingTime } mins to cook.</p>
+        <h3>Ingredients:</h3>
+
+        {ingredients.map(ing => (
+          <span key={ing}>{ ing }</span>
+        ))}
+      </div>
+        
       <div className="method">
         <h3>Method:</h3>
-          <div>{documentToReactComponents(method)}</div>
+        <div>{documentToReactComponents(method)}</div>
       </div>
 
       <style jsx>{`
-         .card {
-          transform: rotateZ(-1deg);
-        }
-        .content {
-          background: #fff;
-          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-          margin: 0;
-          position: relative;
-          top: -40px;
-          left: -10px;
-        }
-        .info {
-          padding: 16px;
-        }
-        .info h4 {
-          margin: 4px 0;
+        h2,h3 {
           text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
         }
         .info p {
           margin: 0;
-          color: #777;
         }
-        .actions {
-          margin-top: 20px;
-          display: flex;
-          justify-content: flex-end;
+        .info span::after {
+          content: ", ";
         }
-        .actions a {
-          color: #fff;
-          background: #f01b29;
-          padding: 16px 24px;
-          text-decoration: none;
+        .info span:last-child::after {
+          content: ".";
         }
-      `}
-      </style>
+      `}</style>
     </div>
   )
 }
